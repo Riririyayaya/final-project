58057f0234b1d5829d7304f2527771c0be03d453
Riririyayaya <89630637+Riririyayaya@users.noreply.github.com>
2021-12-17 12:36:57 +0530

    Create app.js

--- app.js
+++ app.js
@@ -5,4 +5,26 @@ const game = () => {
 	let playerScore = 0;
 	let computerScore = 0;
 	let moves = 0;
+	
+	// Function to
+	const playGame = () => {
+		const rockBtn = document.querySelector('.rock');
+		const paperBtn = document.querySelector('.paper');
+		const scissorBtn = document.querySelector('.scissor');
+		const playerOptions = [rockBtn,paperBtn,scissorBtn];
+		const computerOptions = ['rock','paper','scissors']
+		
+		// Function to start playing game
+		playerOptions.forEach(option => {
+			option.addEventListener('click',function(){
 
+				const movesLeft = document.querySelector('.movesleft');
+				moves++;
+				movesLeft.innerText = `Moves Left: ${10-moves}`;
+				
+
+				const choiceNumber = Math.floor(Math.random()*3);
+				const computerChoice = computerOptions[choiceNumber];
+
+				// Function to check who wins
+				winner(this.innerText,computerChoice)
