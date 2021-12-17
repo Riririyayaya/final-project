c7ea07d056decaf73fc09e7011d470772f3bfbf1
akashjami83 <89630564+akashjami83@users.noreply.github.com>
2021-12-17 12:38:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,3 +36,49 @@ const game = () => {
 		})
 		
 	}
+
+	// Function to decide winner
+	const winner = (player,computer) => {
+		const result = document.querySelector('.result');
+		const playerScoreBoard = document.querySelector('.p-count');
+		const computerScoreBoard = document.querySelector('.c-count');
+		player = player.toLowerCase();
+		computer = computer.toLowerCase();
+		if(player === computer){
+			result.textContent = 'Tie'
+		}
+		else if(player == 'rock'){
+			if(computer == 'paper'){
+				result.textContent = 'Computer Won';
+				computerScore++;
+				computerScoreBoard.textContent = computerScore;
+
+			}else{
+				result.textContent = 'Player Won'
+				playerScore++;
+				playerScoreBoard.textContent = playerScore;
+			}
+		}
+		else if(player == 'scissors'){
+			if(computer == 'rock'){
+				result.textContent = 'Computer Won';
+				computerScore++;
+				computerScoreBoard.textContent = computerScore;
+			}else{
+				result.textContent = 'Player Won';
+				playerScore++;
+				playerScoreBoard.textContent = playerScore;
+			}
+		}
+		else if(player == 'paper'){
+			if(computer == 'scissors'){
+				result.textContent = 'Computer Won';
+				computerScore++;
+				computerScoreBoard.textContent = computerScore;
+			}else{
+				result.textContent = 'Player Won';
+				playerScore++;
+				playerScoreBoard.textContent = playerScore;
+			}
+		}
+	}
